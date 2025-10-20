9f8819702189461da4f327cffff6be13e6f85eb8
Soto <cosoto1@cps.k12.il.us>
2025-10-20 13:34:27 -0500

    first commit

---
+++ python_basics/lesson.py
@@ -0,0 +1,25 @@
+#This is comment in Python
+#Varibles are used to store values
+x = 5 
+y = 3.14 
+Name = "Caiden"
+# a letter is a list of chereter in
+# quation marks
+is_student = True
+# print the values of the Varibles 
+print(x)
+print(y)
+print(Name)
+print(is_student)
+print("My name is " + Name + ", I am " + str(x) + " years old")
+print (type(x))
+print (type(y))
+print (type(Name))
+print (type(is_student))
+#Lets make a new set of varibles to pratice
+a = 10
+b = 2.5
+c = "Caiden"
+d = False
+print(f"{c} said the problem was {d} the asnwer wasn't {b}, it's was actully {a}")
+print(f"My name is {c}, I have {a} apples.")
